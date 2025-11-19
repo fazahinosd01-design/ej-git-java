ae2983345ef07e89114aaff57aca54856f8dcfac
Paco <fazahinosd01@iesalbarregas.es>
2025-11-19 18:31:04 +0100

    funcion tal

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    public static funcion() {
+        System.out.println("esto es una funcion");
+    }
+}
